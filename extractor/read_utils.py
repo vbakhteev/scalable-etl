4b5fd6b5efb41a4ab9e19c130f4d004787ea72d7
vladbakhteev <bakhteev_v@mail.ru>
2022-04-03 21:18:53 +0500

    Fix reading data from tmx

--- extractor/read_utils.py
+++ extractor/read_utils.py
@@ -3,8 +3,9 @@ from lxml import etree
 
 def extract_segments(file_path):
     for event, element in etree.iterparse(file_path, tag='tu'):
-        src, src_lang = extract_text_and_lang(element[4])
-        dst, dst_lang = extract_text_and_lang(element[5])
+        tuv1, tuv2 = element.xpath('tuv')
+        src, src_lang = extract_text_and_lang(tuv1)
+        dst, dst_lang = extract_text_and_lang(tuv2)
 
         yield src, src_lang, dst, dst_lang
 
